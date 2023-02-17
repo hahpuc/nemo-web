93516ddbb98487e4d6d123e86446339a18cc2370
Long Nguyen <18520092@gm.uit.edu.vn>
2023-02-17 16:18:15 +0700

    Update source web nemo

--- web/lucky-wheel/environment.js
+++
@@ -1,8 +0,0 @@
-ZALO_APP_ID = '1494399739217087204'
-ZALO_STATE = "access_profile"
-ZALO_REDIRECT_URI = "https://uloyalty.friso.com.vn/lucky-wheel/login-zalo.html"
-ZALO_SECRET_KEY = "fj5quR272DOKkY2nCpnJ"
-BASE_API = 'https://uapi-loyalty.friso.com.vn'
-CLIENT_ID = 'b8ac13c4-ae4a-11eb-8529-0242ac130003'
-CLIENT_SECRET = '4538EoEDsgeHapav38UTmKD1cLtlEMMy'
-OA_ID = '3364769305613764663'
